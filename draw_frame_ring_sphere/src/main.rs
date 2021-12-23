3ae15128968e5b093afea31556151db6c5a5bbf8
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2021-12-23 19:12:35 +0900

    球を描画する際のstack方向の円の描画.

---
+++ draw_frame_ring_sphere/src/main.rs
@@ -0,0 +1,39 @@
+
+mod draw_frame_sphere;
+
+use glfw::{Action, Context, Key};
+use draw_frame_sphere::*;
+
+fn main() {
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+    let (mut window, events) = glfw.create_window(800, 600, "Draw Frame Sphere", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    window.make_current();
+    window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+
+    let glrender = create_glrender(|s| window.get_proc_address(s) as *const _);
+
+    while !window.should_close() {
+	glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            handle_window_event(&mut window, event);
+        }
+
+	// rendering
+	let (width,height) = window.get_size();
+	glrender.render(width,height);
+	window.swap_buffers();
+    }
+}
+
+fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
+    match event {
+        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+            window.set_should_close(true)
+        },
+        _ => {}
+    }
+}
