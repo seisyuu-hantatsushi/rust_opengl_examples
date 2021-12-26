e2a176933aa8c00d22eeb4e193bc524ecafe4f2f
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2021-12-26 19:07:28 +0900

    球の描画まだ途中.

--- draw_sphere/src/draw_sphere.rs
+++ draw_sphere/src/draw_sphere.rs
@@ -3,7 +3,7 @@ use std::{ptr, str, mem};
 use std::{f64::consts::PI};
 use gl;
 use gl::types::*;
-use linear_transform::{vector::*};
+use linear_transform::{vector::*,matrix::Matrix4x4};
 use graphic_math::graphic_math;
 
 static VERTEX_SHADER_CODE: &'static str = include_str!("simple_viewport.vert");
@@ -116,7 +116,7 @@ static COORDINATE_AXES_INDEX_DATA: [[GLuint;2];3] = [
 ];
 
 static COORDINATE_AXES_COLOR_DATA: [[GLfloat;4];6] = [
-    [ 1.0, 0.0, 0.0, 0.0 ],
+    [ 1.0, 0.0, 0.0, 0.0 ], //x軸 赤
     [ 0.0, 0.0, 0.0, 0.0 ],
     [ 0.0, 1.0, 0.0, 0.0 ],
     [ 0.0, 0.0, 0.0, 0.0 ],
@@ -226,7 +226,7 @@ unsafe fn create_coordinate_axes_array(vao:GLuint) -> VertexArrayObjectContext {
 }
 
 unsafe fn create_sphere_array_object(vao:GLuint) ->  VertexArrayObjectContext {
-    let mut vbos : [GLuint;3] = [0,0,0];
+    let mut vbos : [GLuint;4] = [0,0,0,0];
 
     enum LocationInShader {
 	Position = 0,
@@ -236,14 +236,15 @@ unsafe fn create_sphere_array_object(vao:GLuint) ->  VertexArrayObjectContext {
 
     enum VBOIndex {
 	Vertex = 0,
-	ElementIndex = 1,
-	Color = 2
+	Normal = 1,
+	ElementIndex = 2,
+	Color = 3
     }
 
-    let (circle_vertices, circle_normals, circle_indices) = sphere_vertices(0.5, 24, 24);
+    let (circle_vertices, circle_normals, circle_indices) = sphere_vertices(1.0, 24, 24);
 
     gl::BindVertexArray(vao);
-    gl::GenBuffers(3, &mut vbos[0]);
+    gl::GenBuffers(4, &mut vbos[0]);
 
     gl::BindBuffer(gl::ARRAY_BUFFER, vbos[VBOIndex::Vertex as usize]);
     gl::BufferData(gl::ARRAY_BUFFER,
@@ -260,6 +261,26 @@ unsafe fn create_sphere_array_object(vao:GLuint) ->  VertexArrayObjectContext {
 	    *x = p[0] as GLfloat;
 	    *y = p[1] as GLfloat;
 	    *z = p[2] as GLfloat;
+	    i+=3;
+	}
+	gl::UnmapBuffer(gl::ARRAY_BUFFER);
+    }
+
+    gl::BindBuffer(gl::ARRAY_BUFFER, vbos[VBOIndex::Normal as usize]);
+    gl::BufferData(gl::ARRAY_BUFFER,
+                   (circle_normals.len() * 3 * mem::size_of::<GLfloat>()) as GLsizeiptr,
+		   ptr::null(),
+                   gl::STATIC_DRAW);
+    {
+	let mapped_buffer: *mut GLfloat = gl::MapBuffer(gl::ARRAY_BUFFER, gl::WRITE_ONLY) as *mut GLfloat;
+	let mut i:isize = 0;
+	for n in circle_normals.iter() {
+	    let x: *mut GLfloat = mapped_buffer.offset(i+0);
+	    let y: *mut GLfloat = mapped_buffer.offset(i+1);
+	    let z: *mut GLfloat = mapped_buffer.offset(i+2);
+	    *x = n[0] as GLfloat;
+	    *y = n[1] as GLfloat;
+	    *z = n[2] as GLfloat;
 	    i+=3;
 	}
 	gl::UnmapBuffer(gl::ARRAY_BUFFER);
@@ -303,14 +324,21 @@ unsafe fn create_sphere_array_object(vao:GLuint) ->  VertexArrayObjectContext {
 	gl::UnmapBuffer(gl::ELEMENT_ARRAY_BUFFER);
     }
 
-    //simple_viewport.vertの変数"position"と頂点バッファを結びつける.
+    //lighting.vertの変数"position"と頂点バッファを結びつける.
     gl::EnableVertexAttribArray(LocationInShader::Position as GLuint);
     gl::BindBuffer(gl::ARRAY_BUFFER, vbos[VBOIndex::Vertex as usize]);
     //shader側のメモリに配置したVertex情報のフォーマットを設定する.
     //今回は3次元座標なので,size=3
     gl::VertexAttribPointer(LocationInShader::Position as GLuint, 3, gl::FLOAT, gl::FALSE as GLboolean, 0, ptr::null());
 
-    //simple_viewport.vertの変数"vertexColor"と頂点での色情報バッファを結びつける.
+    //lighting.vertの変数"normal"と法線バッファを結びつける.
+    gl::EnableVertexAttribArray(LocationInShader::Normal as GLuint);
+    gl::BindBuffer(gl::ARRAY_BUFFER, vbos[VBOIndex::Normal as usize]);
+    //shader側のメモリに配置したVertex情報のフォーマットを設定する.
+    //今回は3次元座標なので,size=3
+    gl::VertexAttribPointer(LocationInShader::Normal as GLuint, 3, gl::FLOAT, gl::FALSE as GLboolean, 0, ptr::null());
+
+    //lighting.vertの変数"vertexColor"と頂点での色情報バッファを結びつける.
     gl::EnableVertexAttribArray(LocationInShader::Color as GLuint);
     gl::BindBuffer(gl::ARRAY_BUFFER, vbos[VBOIndex::Color as usize]);
     //shader側のメモリに配置したVertex情報のフォーマットを設定する.
@@ -404,30 +432,45 @@ impl GlRender {
 	let pers   = graphic_math::perspective(30.0, aspect, 1.0, 11.0);
 	let mvp    = pers*lookat;
 	unsafe {
-	    let mvp_str = CString::new("mvp").unwrap_or_else(|_| panic!("failed to allocate string space"));
-	    let mvp_str_ptr = mvp_str.as_ptr();
-	    let mvp_location = gl::GetUniformLocation(self.shader_programs[0], mvp_str_ptr);
+
 	    gl::ClearColor(0.3, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT|gl::DEPTH_BUFFER_BIT);
 	    gl::Viewport(0, 0, width, height);
+
 	    gl::UseProgram(self.shader_programs[0]);
-	    gl::ProgramUniformMatrix4fv(self.shader_programs[0], mvp_location, 1, gl::TRUE, mem::transmute(&mvp.serialize_f32()[0]));
+	    {
+		let mvp_str = CString::new("mvp").unwrap_or_else(|_| panic!("failed to allocate string space"));
+		let mvp_location = gl::GetUniformLocation(self.shader_programs[0], mvp_str.as_ptr());
+		gl::ProgramUniformMatrix4fv(self.shader_programs[0], mvp_location, 1, gl::TRUE, mem::transmute(&mvp.serialize_f32()[0]));
 
-	    gl::BindVertexArray(self.vertex_array_object_contexts[0].vao);
-	    gl::DrawElements(self.vertex_array_object_contexts[0].draw_mode,
-			     self.vertex_array_object_contexts[0].count_of_draw_index,
-			     gl::UNSIGNED_INT, ptr::null());
-	    gl::BindVertexArray(0);
-
+		gl::BindVertexArray(self.vertex_array_object_contexts[0].vao);
+		gl::DrawElements(self.vertex_array_object_contexts[0].draw_mode,
+				 self.vertex_array_object_contexts[0].count_of_draw_index,
+				 gl::UNSIGNED_INT, ptr::null());
+		gl::BindVertexArray(0);
+	    }
 
 	    gl::Enable(gl::DEPTH_TEST);
 	    gl::UseProgram(self.shader_programs[1]);
-	    gl::ProgramUniformMatrix4fv(self.shader_programs[1], mvp_location, 1, gl::TRUE, mem::transmute(&mvp.serialize_f32()[0]));
-	    gl::BindVertexArray(self.vertex_array_object_contexts[1].vao);
-	    gl::DrawElements(self.vertex_array_object_contexts[1].draw_mode,
-			     self.vertex_array_object_contexts[1].count_of_draw_index,
-			     gl::UNSIGNED_INT, ptr::null());
-	    gl::BindVertexArray(0);
+	    {
+		let model_str = CString::new("model").unwrap_or_else(|_| panic!("failed to allocate string space"));
+		let view_str = CString::new("view").unwrap_or_else(|_| panic!("failed to allocate string space"));
+		let projection_str = CString::new("projection").unwrap_or_else(|_| panic!("failed to allocate string space"));
+		let model_location = gl::GetUniformLocation(self.shader_programs[1], model_str.as_ptr());
+		let view_location = gl::GetUniformLocation(self.shader_programs[1], view_str.as_ptr());
+		let projection_location = gl::GetUniformLocation(self.shader_programs[1], projection_str.as_ptr());
+		let translate = graphic_math::translate(Vector3(0.25,-0.25,0.0))*graphic_math::scale(Vector3(0.3,0.3,0.3));
+
+		gl::ProgramUniformMatrix4fv(self.shader_programs[1], model_location, 1, gl::TRUE, mem::transmute(&translate.serialize_f32()[0]));
+		gl::ProgramUniformMatrix4fv(self.shader_programs[1], view_location, 1, gl::TRUE, mem::transmute(&lookat.serialize_f32()[0]));
+		gl::ProgramUniformMatrix4fv(self.shader_programs[1], projection_location, 1, gl::TRUE, mem::transmute(&pers.serialize_f32()[0]));
+
+		gl::BindVertexArray(self.vertex_array_object_contexts[1].vao);
+		gl::DrawElements(self.vertex_array_object_contexts[1].draw_mode,
+				 self.vertex_array_object_contexts[1].count_of_draw_index,
+				 gl::UNSIGNED_INT, ptr::null());
+		gl::BindVertexArray(0);
+	    }
 	    gl::Disable(gl::DEPTH_TEST);
 	    gl::Flush();
 	}
