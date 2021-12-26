0d388aa453dfc2bf9f7cd65967a919c70be2aba5
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2021-12-26 19:05:32 +0900

    生成物を登録してしまったので削除します.

--- graphic_math/target/debug/deps/graphic_math-645d82556a9844a0.d
+++
@@ -1,8 +0,0 @@
-/home/kaz/work/github/seisyuu-hantatsushi/rust_opengl_examples/graphic_math/target/debug/deps/graphic_math-645d82556a9844a0.rmeta: src/lib.rs src/graphic_math.rs
-
-/home/kaz/work/github/seisyuu-hantatsushi/rust_opengl_examples/graphic_math/target/debug/deps/libgraphic_math-645d82556a9844a0.rlib: src/lib.rs src/graphic_math.rs
-
-/home/kaz/work/github/seisyuu-hantatsushi/rust_opengl_examples/graphic_math/target/debug/deps/graphic_math-645d82556a9844a0.d: src/lib.rs src/graphic_math.rs
-
-src/lib.rs:
-src/graphic_math.rs:
